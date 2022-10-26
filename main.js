f9d534e7b19179b35559f03d11ad73bd36c7fd0e
TeaChareqishvili <teachareqishvili89@gmail.com>
2022-10-27 00:55:01 +0400

    array

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+const arrayNum = [2,4,7,1,3];
+
+// console.log(arrayNum);
+// console.log(arrayNum.length);
+// console.log(summary);
+// console.log(sum);
+
+let sum  = arrayNum.length;
+const summary = arrayNum[0] + arrayNum[1] + arrayNum[2] + arrayNum[3] + arrayNum[4];
+const result = summary / sum;
+
+// console.log(result);
+
